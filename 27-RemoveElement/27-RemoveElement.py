6efd97c0c038440a55a43f6444d9df2ad3a9d8b9
Aishwarya Naidu <officialaishu@gmail.com>
2025-08-23 04:13:13 -0700

    File updated at 8/23/2025, 4:13:13 AM

--- 27-RemoveElement/27-RemoveElement.py
+++ 27-RemoveElement/27-RemoveElement.py
@@ -1,15 +1,15 @@
-# Last updated: 8/23/2025, 4:08:35 AM
+# Last updated: 8/23/2025, 4:13:13 AM
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # Writer pointer, it will stop at invalid positions.
+        # k - write pointer, it will stop at invalid positions.
         # invalid = element "val" (which has to be removed)
         # Then later the element gets overwritten thats why its writer pointer
         k = 0
         n = len(nums)
 
-        for i in range(n):
+        for i in range(n): # i - read pointer
             if (nums[i] != val):
-                nums[k] = nums[i]
+                nums[k] = nums[i] # copy valid element
                 k += 1
         
         return k
