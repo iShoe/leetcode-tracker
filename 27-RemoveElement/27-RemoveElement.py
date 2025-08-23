fccf5ece1a9eb7406bc4e6f1c6613bd21cc75090
Aishwarya Naidu <officialaishu@gmail.com>
2025-08-23 04:08:36 -0700

    File updated at 8/23/2025, 4:08:35 AM

--- 27-RemoveElement/27-RemoveElement.py
+++ 27-RemoveElement/27-RemoveElement.py
@@ -1,6 +1,9 @@
-# Last updated: 8/23/2025, 3:26:58 AM
+# Last updated: 8/23/2025, 4:08:35 AM
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # Writer pointer, it will stop at invalid positions.
+        # invalid = element "val" (which has to be removed)
+        # Then later the element gets overwritten thats why its writer pointer
         k = 0
         n = len(nums)
 
