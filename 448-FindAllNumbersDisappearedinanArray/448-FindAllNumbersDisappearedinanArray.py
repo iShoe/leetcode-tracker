6d0b030e8cd56a895785fa91a8876dacc8c40122
Aishwarya Naidu <officialaishu@gmail.com>
2025-08-24 00:51:42 -0700

    File updated at 8/24/2025, 12:51:41 AM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
@@ -1,13 +1,28 @@
-# Last updated: 8/24/2025, 12:42:58 AM
+# Last updated: 8/24/2025, 12:51:41 AM
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        hset = set(nums)
+        # hset = set(nums)
+        # result = []
+
+        # for i in range(1, len(nums)+1):
+        #     if i not in hset:
+        #         result.append(i)
+        
+        # return result
+
+        # No extra space solution
+
         result = []
 
-        for i in range(1, len(nums)+1):
-            if i not in hset:
-                result.append(i)
+        for i in range(len(nums)):
+            n = abs(nums[i])
+            index = n - 1
+            if nums[index] > 0:
+                nums[index] *= -1
         
-        return result
+        for i in range(len(nums)):
+            if(nums[i] > 0):
+                result.append(i+1)
 
+        return result
         
