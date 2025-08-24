f9b6a94e16a9533cbb9c7447f057dcaf1d5b6e58
Aishwarya Naidu <officialaishu@gmail.com>
2025-08-24 00:05:51 -0700

    File updated at 8/24/2025, 12:05:51 AM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,17 +1,25 @@
-# Last updated: 8/24/2025, 12:05:26 AM
+# Last updated: 8/24/2025, 12:05:51 AM
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
+        # [-1, 0, 3, 5, 9, 12]
+        #    0 1 2 3 4 5
+        # len = 9 
+
         start = 0
         end = len(nums) - 1
-        
-        while start <= end: 
+        mid = 0
+
+        while (start <= end):
             mid = start + ((end - start) // 2)
+
+            if nums[mid] > target:
+                end = mid-1
             
-            if nums[mid] == target:  
+            elif nums[mid] < target:
+                start = mid+1
+            else:
                 return mid
-            elif nums[mid] > target:
-                end = mid - 1
-            else:  # nums[mid] < target
-                start = mid + 1
-        
+
+            
         return -1
